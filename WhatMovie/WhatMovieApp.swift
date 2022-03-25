9672338f1995fa36ec9cc50e8a646a6831f814d6
Gino Tasis <gtasis84@gmail.com>
2022-03-25 14:28:31 -0400

    Initial Commit

---
+++ WhatMovie/WhatMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatMovieApp.swift
+//  WhatMovie
+//
+//  Created by Gino Tasis on 3/25/22.
+//
+
+import SwiftUI
+
+@main
+struct WhatMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
