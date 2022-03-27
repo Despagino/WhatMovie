06c3a456699ff20ca01ac00d120db227ce95840e
Gino Tasis <gtasis84@gmail.com>
2022-03-27 00:13:49 -0400

    Complete url session

--- WhatMovie/MovieStore.swift
+++ WhatMovie/MovieStore.swift
@@ -43,8 +43,44 @@ class MovieStore: MovieService {
             )
         }
         
+        urlComponents.queryItems = queryItems
+        guard let finalURL = urlComponents.url else {
+            completion(.failure(.invalidEndpoint))
+            return
+        }
+        
+        urlSession.dataTask(with: finalURL) { data, response, error in
+            
+            if error != nil {
+                completion(.failure(.apiError))
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse, 200..<300 ~= httpResponse.statusCode else {
+                self.executeCompletionHandler(with: .failure(.invalidResponse), completion: completion)
+                return
+            }
+            
+            guard let data = data else {
+                self.executeCompletionHandler(with: .failure(.noData), completion: completion)
+                return
+            }
+            
+            do {
+                let decodedResponse = try self.jsonDecoder.decode(D.self, from: data)
+                self.executeCompletionHandler(with: .success(decodedResponse), completion: completion)
+            } catch {
+                self.executeCompletionHandler(with: .failure(.serializationError), completion: completion)
+            }
+        }.resume()
+        
     }
     
-    
+    private func executeCompletionHandler<D: Decodable>(with result: Result<D, MovieError>, completion: @escaping (Result<D, MovieError>) -> ()) {
+        
+        DispatchQueue.main.async {
+            completion(result)
+        }
+        
+    }
     
 }
